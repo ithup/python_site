ead74fa0a32e7497db2f417bf212ba6d26afef34
ithuplion <1805421482@qq.com>
2017-07-13 22:58:06 +0800

    html2

--- urllib/cto.py
+++ urllib/cto.py
@@ -1,10 +1,10 @@
 #Author:ithuplion
 import urllib.request
-file=urllib.request.urlopen("http://v3.bootcss.com/")
+file=urllib.request.urlopen("http://www.jxhkst.com")
 data=file.read()
 dataline=file.readline()
 # print(dataline)
 # print(data)
-fhandle=open("d:/Python35/myweb/part4/bootstrap.html","wb")
+fhandle=open("d:/Python35/myweb/part4/huakai.html","wb")
 fhandle.write(data)
 fhandle.close()
